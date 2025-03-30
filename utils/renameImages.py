51f1e4a7c7c3b965cfbe46030f40d79c86bada6d
Olga Popova <mushroom.1661@gmail.com>
2025-03-30 17:30:25 -0500

    subject goes first

--- utils/renameImages.py
+++ utils/renameImages.py
@@ -1,6 +1,6 @@
 import os
 
-folderPath = "datacat/class" 
+folderPath = "datacat/instance" 
 className = "cat"
 
 files = sorted(os.listdir(folderPath))
@@ -8,12 +8,12 @@ files = sorted(os.listdir(folderPath))
 for i, filename in enumerate(files):
     fileExtension = os.path.splitext(filename)[1]
     if fileExtension.lower() in [".jpg", ".jpeg", ".png", ".webp"]:
-        newFileName = f"{className} {i:03}{fileExtension}"
+        newFileName = f"Mr Beans {className} {i:03}{fileExtension}"
         oldPath = os.path.join(folderPath, filename)
         newPath = os.path.join(folderPath, newFileName)
         os.rename(oldPath, newPath)
 
 # Utility script for renaming images for DreamBooth training.
 # Both class and instance images should follow a consistent naming pattern.
-# Format for instance images: <class> <subject> <index>, e.g., "cat mr beans 001.jpg"
+# Format for instance images: <subject> <class> <index>, e.g., "Mr Beans cat 001.jpg"
 # Format for class images: <class> <index>, e.g., "cat 001.jpg"
